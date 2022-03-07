588a681d6fe71f7c65dfca6d7a44d781e7619676
tawfiq khalilieh <87164562+xtawfeeq7k@users.noreply.github.com>
2022-03-07 20:46:21 +0200

    Update __init__.py

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -4,8 +4,10 @@ from app.config import settings
 
 class Dynamo:
     _instances = {
-
+        'created_users':[]
     }
+
+    # crate response
     def create_reso(self):
         try:
             if "resource" not in self._instances:
@@ -18,6 +20,7 @@ class Dynamo:
         except ClientError as e:
             raise e
 
+    # create table
     def create_table(self):
         try:
             if "created_table" not in self._instances:
@@ -46,13 +49,55 @@ class Dynamo:
         except ClientError as e:
             pass
 
+    # create user (should edit)
+    def create_user(self,user):
+        try:
+            if user.id not in self._instances['created_users']:
+                dynamodb = self.create_reso()
+                table = dynamodb.Table('users')
+                response = table.get_item(
 
-    def get_table(self):
-        if "table" not in self._instances:
+                    Key={
+
+                        'id': user.id,
+                        'username': user.username,
+                        'email': str(user.email),
+                        'password': user.password,
+                        'age': user.age,
+                        'gender': user.gender,
+                        'birthday': str(user.birthday)
+
+
+                    }
+
+                )
+
+                if 'Item' in response:
+
+                    return response['Item']
+
+                else:
+
+                    return {
+
+                        'statusCode': '404',
+
+                        'body': 'Not found'
+
+                    }
+            else:
+                return {'Error':'User in already exists'}
+
+        except ClientError as e:
+            pass
+
+
+    def get_users_table(self):
+        if "users" not in self._instances:
             dynamodb = self.create_reso()
             table = dynamodb.Table(settings.table)
-            self._instances["table"] = table
-        return self._instances["table"]
+            self._instances["users"] = table
+        return self._instances["users"]
 
 
 dynamo = Dynamo()
