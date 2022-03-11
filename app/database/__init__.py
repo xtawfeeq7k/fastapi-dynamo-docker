325e93c12f5342e8b3551cbd4364599289f66b3d
tawfiq khalilieh <87164562+xtawfeeq7k@users.noreply.github.com>
2022-03-11 19:56:13 +0200

    Update __init__.py

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -4,9 +4,9 @@ from app.config import settings
 
 class Dynamo:
     _instances = {
-
+        "created_users": []
     }
-    # crate response
+    # create response > working
     def create_reso(self):
         try:
             if "resource" not in self._instances:
@@ -14,91 +14,83 @@ class Dynamo:
                     boto3.resource('dynamodb',
                                    endpoint_url=settings.endpoint_url,
                                    verify=False,
-                                   region_name='dummy')
+                                   region_name='eu-central-1',
+                                   aws_access_key_id="hello",
+                                   aws_secret_access_key = "hello")
                 self._instances["resource"] = dynamodb
             return self._instances["resource"]
         except ClientError as e:
             raise e
 
-
-
+    # create_table > working
     def create_table(self,dynamodb=None):
         try:
             if not dynamodb or "resource" not in self._instances:
                 dynamodb = boto3.resource('dynamodb',
                                    endpoint_url=settings.endpoint_url,
                                    verify=False,
-                                   region_name='dummy')
+                                   region_name='eu-central-1',
+                                  aws_access_key_id="hello",
+                                  aws_secret_access_key="hello")
                 self._instances["resource"] = dynamodb
             return self._instances["resource"]
         except ClientError as e:
             raise "Error"
 
-    def create_table2(self,dynamodb=None):
-        table = dynamodb.create_table(
-            TableName='table',
-            KeySchema=[
-                {
-                    'AttributeName': 'id',
-                    'KeyType': 'HASH'  # Partition key
-                },
-                {
-                    'AttributeName': 'id',
-                    'KeyType': 'S'  # Sort key
-                }
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 10,
-                'WriteCapacityUnits': 10
-            }
-        )
-        self._instances["created_table"] = True
-        return table
-
-    def get_table(self):
+    # get_table > working
+    def get_table(self,dynamodb=None):
         if "table" not in self._instances:
             dynamodb = self.create_reso()
             table = dynamodb.Table(settings.table)
             self._instances["table"] = table
-        return table
+        return self._instances["table"]
 
-    def create_user(self, user):
+    # get_user > working
+    def get_user(self, id, username, dynamodb=None):
+        if not dynamodb:
+            dynamodb = boto3.resource('dynamodb',endpoint_url=settings.endpoint_url,verify=False,region_name='eu-central-1',aws_access_key_id="hello",aws_secret_access_key="hello")
+        table = dynamodb.Table('table')
         try:
-            if user.id not in self._instances['created_users']:
-                dynamodb = self.create_reso()
-                table = dynamodb.Table('table')
-                response = table.get_item(
-
-                    Key={
-
-                        'id': user.id,
-                        'username': user.username,
-                        'email': str(user.email),
-                        'password': user.password,
-                        'age': user.age,
-                        'gender': user.gender,
-                        'birthday': str(user.birthday)
-
-                    }
-
-                )
-
-                if 'Item' in response:
-
-                    return response['Item']
-
-                else:
-
-                    return {
-
-                        'statusCode': '404',
-
-                        'body': 'Not found'
-
-                    }
-            else:
-                return {'Error': 'User in already exists'}
-
+            response = table.get_item(Key={'id': id, 'username': username})
         except ClientError as e:
-            pass
+            print(e.response['Error']['Message'])
+        else:
+            return response['Item']
+
+    # get_all_users > working
+    def get_all_users(self,dynamodb=None,users=[]):
+        if not dynamodb:
+            dynamodb = boto3.resource('dynamodb', endpoint_url=settings.endpoint_url, verify=False,region_name='eu-central-1', aws_access_key_id="hello",aws_secret_access_key="hello")
+        table = dynamodb.Table('table')
+        for user in self._instances["created_users"]:
+            try:
+                response = table.get_item(Key={'id': id})
+                user.append(response)
+            except ClientError as e:
+                print(e.response['Error']['Message'])
+            else:
+                users.append(response['Item'])
+        return users
+
+    def response_test(self):
+        return "hey!"
+
+    # create user > not working ( example )
+    def put_movie(self ,title, year, plot, rating, dynamodb=None):
+        if not dynamodb:
+            dynamodb = boto3.resource('dynamodb', endpoint_url=settings.endpoint_url, verify=False,region_name='eu-central-1', aws_access_key_id="hello",aws_secret_access_key="hello")
+
+        table = dynamodb.Table('table')
+        response = table.put_item(
+            Item={
+                'year': year,
+                'title': title,
+                'info': {
+                    'plot': plot,
+                    'rating': rating
+                }
+            }
+        )
+        return response
+
 dynamo = Dynamo()
