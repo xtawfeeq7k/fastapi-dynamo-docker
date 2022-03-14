ead3a498a7632cf292cb86f21460a5673cf81ba4
tawfiq khalilieh <87164562+xtawfeeq7k@users.noreply.github.com>
2022-03-14 17:57:47 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ import uuid
 # app
 app = FastAPI()
 api_key_header = APIKeyHeader(name='X-API-Key', auto_error=True)
+API_Key = "altooro"
 app_auth = APIRouter()
 
 @app.on_event("startup")
@@ -23,16 +24,46 @@ async def stratup_table():
 
 @app.get("/get/user-by-id-username")
 def get_user(id: str,username: str):
-    dynamo.get_user(id=id,username=username)
-
+    r= dynamo.get_user(id=id,username=username)
+    if r != None:
+        return r
+    else:
+        return {"Error":"User Not Found"}
 @app.get("/get/allusers")
-def getallusers():
-    dynamo.get_all_users()
+def getallusers(APIkey: str = Depends(api_key_header)):
+    if APIkey == "altooro":
+        r = dynamo.get_all_users()
+        if r != None:
+            return r
+    else:
+        return {"Error":"Your API Key is wrong"}
 
-@app.post("/create/user")
-def create_user():
-    dynamo.put_movie(title="hi", year="hh", plot="hi", rating="alo")
+@app.put("/update/username-email")
+def update_username_email(user: update_user_username_email):
+    if get_user(user.id,user.username):
+        try:
+            dynamo.update_username_email(id=user.id,username=user.username,email=user.email)
+            return {"Message":"User Updated Succsessfully"}
+        except:
+            return {"Error":"Somthing Wrong Happen"}
+    else:
+        return {"Error":"User Not Found"}
 
-@app.get("/response-test")
+# not working
+@app.post("/insert/user")
+def insert_items():
+    id = str(uuid.uuid4())
+    dynamo.insert_items(id=id)
+
+
+@app.delete("/delete/user/{id}")
+def delete_user(id: str):
+    return dynamo.delete_user(id)
+
+@app.delete("/delete/all/users")
+def delete_all_users():
+    return dynamo.delete_all_users()
+
+@app.get("/response-test",tags=["test"])
 def responsetest():
     return dynamo.response_test()
