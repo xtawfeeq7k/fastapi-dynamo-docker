9ec8d6f9be7fe24f02ea6b978f27a7dd4ee4f346
tawfiq khalilieh <87164562+xtawfeeq7k@users.noreply.github.com>
2022-03-15 15:46:17 +0200

    table

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
 
   endpoint_url: str = "http://localhost:4566"
-  table : str = "users"
+  table : str = "table"
 
 
 settings = Settings()
